0dfae0b249a277f51581563ba585e7774d5938f7
misobae <misobae0731@gmail.com>
2022-03-10 15:00:39 +0900

    Refactor: Component별로 파일 나누기

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+import Subject from "./components/Subject"
 import './App.css';
+
+
+
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        Hello, React!!
+        <Subject title="WEB" sub="World Wide Web!" />
+        <TOC />
+        <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
     );
   }
