8a863ae0c82afd3add33c7fcca03d06c34374e55
misobae <misobae0731@gmail.com>
2022-03-10 18:37:47 +0900

    Feat: 이벤트 추가

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id: 1,
       subject: {title: 'WEB', sub: 'World Wide Web!'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!'},
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information.'},
         {id: 2, title: 'CSS', desc: 'CSS is for design.'},
@@ -17,14 +20,41 @@ class App extends Component {
     }
   }
   render(){
+    let _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      for(let i = 0; i < this.state.contents.length; i++){
+        let data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+        }
+      }
+    }
+
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}
+          onChangePage={function(){
+            // Component 생성이 끝난 뒤 동적으로 state 값을 바꿀 때 setState 함수 사용.
+            this.setState({
+              mode: 'welcome'
+            });
+          }.bind(this)}
         />
-        <TOC data={this.state.contents} />
-        <Content title="HTML" desc="HTML is HyperText Markup Language." />
+        <TOC 
+          onChangePage={function(id){
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)} data={this.state.contents}
+        />
+        <Content title={_title} desc={_desc} />
       </div>
     );
   }
--- React/react-app/src/components/TOC.js
+++ React/react-app/src/components/TOC.js
@@ -6,7 +6,18 @@ class TOC extends Component {
         const data = this.props.data;
         for(let i = 0; i < data.length; i++){
             // 여러 개의 엘리먼트를 자동으로 생성하는 경우, key prop를 갖고 있어야 한다.
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        data-id={data[i].id}
+                        onClick={function(e){
+                            e.preventDefault();
+                            this.props.onChangePage(e.target.dataset.id);
+                        }.bind(this)}
+                    >{data[i].title}</a>
+                </li>
+            );
         }
         return(
             <nav>
